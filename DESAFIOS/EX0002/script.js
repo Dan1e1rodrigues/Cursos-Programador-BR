3e8cd2fae5f9f07f46083461b6cb229bf8fb5a62
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-04-17 15:19:55 -0300

    commit
    desafio 0002 clicou o alerta apareceu

---
+++ DESAFIOS/EX0002/script.js
@@ -0,0 +1,7 @@
+// Lembrando que eu tenho que criar uma fução!
+// O que quer dizer que devo criar um evento que ocorra quando o botão for clicado
+
+function clicou() {
+    alert('Você apertou o botão')
+}
+
