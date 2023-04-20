beb2b63cef9af2160228ae3a927fc03f1beedac8
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-04-20 20:27:58 -0300

    commit

--- DESAFIOS/EX0004/script.js
+++ DESAFIOS/EX0004/script.js
@@ -1,7 +1,13 @@
 function clicou(){
-    var nome = prompt("Qual e o seu nome");
+    let nome = window.prompt('Qual e o seu nome');
 
-    document.write("Olá " + nome + " E um prazer te conhecer!" )
+    let resul = window.document.getElementById('resultado')
+
+    // O "" e ´´ são diferentes e por isso não funcionou com "" então preste atenção só foi com `´
+
+    resul.innerHTML = `<p>Oi, <strong>${nome}</strong>! E um prazer te conhecer!;`
+
+    // document.write("Olá " + nome + " E um prazer te conhecer!" );
     
 }
 
