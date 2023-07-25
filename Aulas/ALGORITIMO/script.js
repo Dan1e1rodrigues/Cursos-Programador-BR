ff89f380357ab9b58f8e7e178f435cd63ed3fcd3
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-07-25 15:01:03 -0300

    UP
    Aprendendo a usar a variável.

---
+++ Aulas/ALGORITIMO/script.js
@@ -0,0 +1,8 @@
+//Irá aparecer no sonsole
+console.log("Ola Mundo")
+
+//Irá aparecer na tela de forma visual e como Um ! Alerta
+alert("Óla Munhdo")
+
+//Esse e um print da tela, função de Impressora
+//print("Óla")
--- Aulas/JAVASCRIPT--------/script.js
+++ Aulas/JAVASCRIPT--------/script.js
@@ -1,1 +1,8 @@
-alert("Iniciando estudo em javascript ")
+let nota1 = parseInt(prompt("Digite um número"));
+
+let nota2 = parseInt(prompt("Digite outro número"));
+
+var soma = (nota1 + nota2)
+
+document.write("A soma entre " + nota1 + " é " + nota2 + " e igual a " + soma + "");
+
