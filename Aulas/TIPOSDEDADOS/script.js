241f8d6f09c11e802daea8e8ec48fd38cae7b1c2
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-07-26 16:25:40 -0300

    Tipos de dados
    Estudando os tipos de dados, String, null, undefined, number e string

---
+++ Aulas/TIPOSDEDADOS/script.js
@@ -0,0 +1,20 @@
+// Tipos de Dados
+// Null
+// Undefined
+// Number
+// String
+
+/*
+São coisas diferentes
+var a = "11";
+var b = 11;
+
+console.log(a);
+console.log(b);
+
+Aqui trabalhamos com concatenção
+console.log("1" + "1" + " Concatenação");
+*/
+
+var a = "Resultado";
+ console.log(a);
