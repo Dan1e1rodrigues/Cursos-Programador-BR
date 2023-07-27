7b8ac2d7b073c8fd0e898459b9f45543b161fb38
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-07-27 16:25:35 -0300

    Operadores Comparativos.

--- Aulas/OPERACOESARITMEDICAS/script.js
+++ Aulas/OPERACOESARITMEDICAS/script.js
@@ -24,8 +24,53 @@ console.log(soma);
 // /=   x /= y  x = x / y
 // %=   x %= y  x = x % y
 
+/*
 var a = 30;
 
 a += 10;
 
 console.log(a);
+*/
+
+// var num1 = parseInt(prompt("Digite um numero: "));
+// var num2 = parseInt(prompt("Digite outro numero: "));
+
+// var calculo = (num1 + num2);
+
+// document.write("O resultado é " + soma + "")
+
+// var a = "";
+// a = 50;
+//  console.log(a)
+
+// Operadores Comparativos
+
+/*
+
+== igual a 
+=== igual e igual
+!= não é igual
+!== não igual ou não igual
+> maior que
+< menor que
+>= maior que ou igual a
+<= menor ou igual a
+
+*/
+
+//Vai ser TRUE
+// var a = 3 ;
+// var b = 3 ;
+
+// console.log(a === b);
+
+//Vai ser FALSE
+// var a = 3 ;
+// var b = "3" ;
+
+// console.log(a === b);
+
+var a = 3 ;
+var b = 3 ;
+
+console.log(a === b);
