9664099a82eb8b109c2d611c9f12c4d590155835
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-07-27 11:56:44 -0300

    Atribuições.
    Atribuições de valores e exemplos de como fazer de diversas formas, com subtração, adição, multiplicação, modulo e divisão.

--- Aulas/OPERACOESARITMEDICAS/script.js
+++ Aulas/OPERACOESARITMEDICAS/script.js
@@ -5,6 +5,7 @@
 // ++ Incremento
 // -- Decremento
 
+/*
 var a = 20;
 
 var b = 10;
@@ -12,3 +13,19 @@ var b = 10;
 var soma = (a % b);
 
 console.log(soma);
+*/
+
+// ATRIBUIÇÔES
+
+// =    x = y   x = y
+// +=   x += y  x = x + y
+// -=   x -= y  x = x - y
+// *=   x *= y  x = x * y
+// /=   x /= y  x = x / y
+// %=   x %= y  x = x % y
+
+var a = 30;
+
+a += 10;
+
+console.log(a);
