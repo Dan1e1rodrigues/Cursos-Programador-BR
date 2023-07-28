5621fdd881c9ba5c64fabdd056934baea48be987
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-07-28 13:32:57 -0300

    UP

--- Aulas/OPERACOESARITMEDICAS/script.js
+++ Aulas/OPERACOESARITMEDICAS/script.js
@@ -70,7 +70,11 @@ console.log(a);
 
 // console.log(a === b);
 
-var a = 3 ;
-var b = 3 ;
+// var a = 3 ;
+// var b = 3 ;
 
-console.log(a === b);
+// console.log(a === b);
+
+//Operadores Logicos
+
+
