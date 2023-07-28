4815950b0525a9c4fccc64012b2f248a21908518
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-07-28 14:26:33 -0300

    Operadores Logicos.
    Aula de hoje foi sobre || e &&

--- Aulas/OPERACOESARITMEDICAS/script.js
+++ Aulas/OPERACOESARITMEDICAS/script.js
@@ -77,4 +77,42 @@ console.log(a);
 
 //Operadores Logicos
 
+//Estou perguntando o seguinte A e B são iguais ? Não pois um e TRUE e o outro e FALSE
+//Logo o resultado e false pois as duas nn possuem o mesmo valor.
 
+// var a = true;
+// var b = false;
+
+// var c = a && b;
+
+// console.log(c);
+
+// var idade = 35;
+
+// var maiorN = idade > 20;
+// var menorN = idade < 40;
+
+// var entreM = maiorN && menorN;
+
+// console.log("A sua idade é: " + idade + "");
+// console.log("sua idade é maior que: " + maiorN + "");
+// console.log("Sua idade é menor que: " + menorN + "");
+// console.log("Sua idade está entre 20 e 40: " + idade + "");
+ 
+//Faça um codigo que saiba identificar se o usuario pode ter acesso a gratuidade
+// se ele for menor de 10 anos ele tem acesso e se for maior de 65 tmb tem acesso gratuito
+
+var idade = 64;
+
+var menor10 = idade <= 10;
+var maior65 = idade >= 65;
+
+var gratuidadeS = menor10 || maior65;
+
+console.log("A sua idade é: " + idade + "");
+
+console.log("sua idade é maior que 65: " + maior65 + "");
+
+console.log("Sua idade é menor que 10: " + menor10 + "");
+
+console.log("O usuario pode ter acesso a Gratuidade? " + gratuidadeS + "");
