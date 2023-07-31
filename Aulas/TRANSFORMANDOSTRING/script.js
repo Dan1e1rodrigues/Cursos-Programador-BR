48ab1444fb4934ba55af1b446284778b43a3dacd
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-07-31 17:00:54 -0300

    ParseInt

---
+++ Aulas/TRANSFORMANDOSTRING/script.js
@@ -0,0 +1,28 @@
+// Transformando os tipos
+
+// Desse modo concatena
+// var a = "10";
+
+// var b = "30";
+
+// var c = a + b;
+// console.log(c);
+//----------------------------
+
+// Desse Soma
+// var a = 10;
+
+// var b = 30;
+
+// var c = a + b;
+// console.log(c);
+
+var a = "10";
+
+var b = "30";
+
+// ParseInt transforma a variavel em um valor inteiro, assim ao inves de concatenar irá somar
+
+var c = parseInt(a) + parseInt(b);
+
+console.log(c);
