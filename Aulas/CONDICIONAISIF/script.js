864c14be66234a9166c968ce579eb36a9c115b0c
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-08-01 20:43:19 -0300

    Operador ternario

--- Aulas/CONDICIONAISIF/script.js
+++ Aulas/CONDICIONAISIF/script.js
@@ -1,3 +1,5 @@
+/*
+
 var idade = parseInt(prompt("Qual e a sua idade ?"));
 
 if(idade >= 35){
@@ -23,3 +25,11 @@ else{
     window.alert("Ele e menor de idade ")
     window.alert("Volte futuramente AMIGO !!!");
 }
+
+*/
+var idade = 2;
+
+                            //true      false
+var pode = idade >= 18 ? "Pode berber maior de idade !!" : "Não pode beber, menor de idade !!";
+
+console.log(pode);
