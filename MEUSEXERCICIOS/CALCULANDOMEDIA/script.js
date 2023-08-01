5dbfcabb83e45a5d495f78b9a0cec3078ca8bd9d
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-08-01 16:12:01 -0300

    UP
    Fazendo exercícios de javascript.

---
+++ MEUSEXERCICIOS/CALCULANDOMEDIA/script.js
@@ -0,0 +1,8 @@
+var nome = prompt("Qual o nome do Aluno ?");
+
+var nota01 = parseFloat(prompt("Qual foi a primeira nota de " + nome + ""));
+
+var nota02 = parseFloat(prompt("Além de " + nota01 + " qual foi a outra nota de " + nome + ""));
+
+var media = nota01 
+
