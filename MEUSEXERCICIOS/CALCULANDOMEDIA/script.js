39de21566058b1abadfebd01f9e7d3524a270138
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-08-01 16:58:11 -0300

    UP
    Ultimo exercicio do dia, referente a DOM.

--- MEUSEXERCICIOS/CALCULANDOMEDIA/script.js
+++ MEUSEXERCICIOS/CALCULANDOMEDIA/script.js
@@ -1,8 +1,18 @@
 // Para criar uma interação basta criar uma função e colocar a variavel dentreo dela e chamar na função.
-function botao() {
 
-    var nome = window.prompt("Qual e o seu nome ?");
-    window.alert("Olá, " + nome + "E um prazer conhecer você !!!")
+function inicio() {
+
+    let nome = window.prompt('Qual é o seu nome?')
+
+
+    let res = window.document.getElementById('resultado')
+
+    // Também funciona com o comando abaixo
+
+    // let res = window.document.querySelector('section#resultado')
+
+    // Por fim o texto resultante a interação para com o usuario e como bonus eté mesmo um EMOJI
+    res.innerHTML = `<p>Olá, <strong>${nome}</strong>! É um grande prazer te conhecer! &#x1F596;`
 }
 // Minha primeira tentativa estava correta porém algo deu errado o alert não estava sendo ativado edequadamente. 
 
