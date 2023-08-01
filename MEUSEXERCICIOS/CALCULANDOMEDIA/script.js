acfe8d8867cc76b1fc7f53d7184515c5cc2220f4
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-08-01 16:36:00 -0300

    UP
    Exercicio 02

--- MEUSEXERCICIOS/CALCULANDOMEDIA/script.js
+++ MEUSEXERCICIOS/CALCULANDOMEDIA/script.js
@@ -1,9 +1,10 @@
 // Para criar uma interação basta criar uma função e colocar a variavel dentreo dela e chamar na função.
 function botao() {
 
-    alert("Você apertou no botão");    
-    
+    var nome = window.prompt("Qual e o seu nome ?");
+    window.alert("Olá, " + nome + "E um prazer conhecer você !!!")
 }
+// Minha primeira tentativa estava correta porém algo deu errado o alert não estava sendo ativado edequadamente.
 
 
 
