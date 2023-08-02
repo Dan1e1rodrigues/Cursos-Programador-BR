ee44279cbc0e41df006c986655cd4437259784f0
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-08-02 14:33:20 -0300

    UP
    Exercicios.

---
+++ MEUSEXERCICIOS/TREINAMENTO/script.js
@@ -0,0 +1,22 @@
+let A = parseInt(prompt("Digite o primeiro número: "));
+
+let B = parseInt(prompt("Digite o segundo número: "));
+
+let C = parseInt(prompt("Digite o terceiro número: "));
+
+if ((A > B) && (A > C)) mensagem = "O PRIMEIRO número informado é maior!"
+else
+
+if ((B > A) && (B > C)) mensagem = "O Segundo número informado é maior!"
+else
+
+if ((C > A) && (C > B)) mensagem = "O TERCEIRO número informado é maior!"
+else
+
+mesagem = "Os números informados são iguais ou nada foi informado.";
+
+alert(mensagem);
+
+
+
+
