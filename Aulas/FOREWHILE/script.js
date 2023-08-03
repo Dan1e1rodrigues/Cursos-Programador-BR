3e9717b9e0233fb9fa6640f9adcf473e8ce2f903
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-08-03 20:14:48 -0300

    For e while

---
+++ Aulas/FOREWHILE/script.js
@@ -0,0 +1,21 @@
+// var numero = 5;
+//  for(var vez = 0; vez < numero; vez ++){
+
+//     console.log("Executando o FOR, pela " + vez + " vez");
+//  }
+
+//  console.log("Acabou...");
+
+//Looping infinito (CUIDADO).
+
+/*
+var numero = 5;
+
+while(numero < 10){
+
+    console.log("Número " + numero + "");
+
+}
+
+console.log("Acabou");
+*/
