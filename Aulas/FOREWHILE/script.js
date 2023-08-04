b5ee102f726aa5db970b949320920fd0b7efbd0d
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-08-03 21:09:10 -0300

    While e for

--- Aulas/FOREWHILE/script.js
+++ Aulas/FOREWHILE/script.js
@@ -19,3 +19,35 @@ while(numero < 10){
 
 console.log("Acabou");
 */
+
+//Array
+
+// Lembrando que o Array começa com letra maiuscula.
+
+// var alunos = new Array ("Daniel", "Rian", "Henrique", "Guilherme");
+
+// Esse irá mostrar o objeto na posição 0
+
+// console.log(alunos[0]);
+
+// Já esse TODOS
+
+// console.log(alunos);
+
+/*
+
+var alunos = new Array ("Daniel", "Rian", "Henrique", "Guilherme", "Bernado");
+
+console.log(alunos[0]);
+console.log(alunos[1]);
+console.log(alunos[2]);
+console.log(alunos[3]);
+console.log(alunos[4]);
+
+*/
+
+var alunos = new Array ("Daniel","Rian","Henrique","Guilherme","Bernado","Nivaldo","Girafales","Aguiar");
+
+for(var i = 0; i < alunos.length; i++){
+    console.log(alunos[i]);
+}
