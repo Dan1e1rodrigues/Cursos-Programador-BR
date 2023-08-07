715d82831cad59ef451e649bbda8614ea485c9d3
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-08-07 16:51:35 -0300

    Calculando Media.
    Exercitando novamente essa atividade, mas dessa vez aplicando todos os conceitos que aprendi durante o curso, como function, tipos de dados, condicionais, e tipos primitivos.

--- Aulas/FUNCCAO/script.js
+++ Aulas/FUNCCAO/script.js
@@ -1,3 +1,4 @@
+/*
 function ativa(){
     window.alert("Você apertou o botão");
 
@@ -32,3 +33,28 @@ var media = (n1, n2) => {
 var m = media(5, 6);
 
 console.log(m)
+*/
+
+// Desafio calculando media, colocar nome do aluno, media1 e media2, media final e se foi aprovado ou não. 
+
+function ativa(){
+    var nome = window.prompt("Qual é o seu nome ?");
+
+    let primeiraNota = Number(window.prompt("" + nome +" qual foi a sua primeira nota ?"));
+
+    let segundaNota = Number(window.prompt("Agora a sua segunda nota ?"));
+
+    let media = (primeiraNota + segundaNota) / 2;
+
+    let res = document.querySelector('section#res')
+
+    if(media >= 6){
+        window.alert("APROVADO")
+    }else{
+        window.alert("REPROVADO")
+    }
+
+    res.innerHTML = `<p>Calculando a média final de <mark>${nome}</mark>.</p>`
+    res.innerHTML += `<p>As notas obtidas foram <mark>${primeiraNota} e ${segundaNota}</mark>.</p>` 
+    res.innerHTML += `<p>A média final será <mark>${media}</mark>.</p>` 
+}
