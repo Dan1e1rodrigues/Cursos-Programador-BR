a98096375e51b5e5d2c00e27090f870edc18d892
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-08-07 20:48:54 -0300

    Tipos de objetos.
    Na aula de hoje foi desenvolvida a criação de um objeto.

---
+++ Aulas/OBJETOS/script.js
@@ -0,0 +1,34 @@
+// var objetos = ["Daniel", "Rian", "Helena"];
+
+//Vai mostrar o valor da posição
+// document.write([2]);
+//Mostra a posição do elemento nesse caso vai mostrar o nome
+// document.write(objetos[2]);
+
+//Em um objeto e e diferença e de {}
+
+//OBJETO...
+var aluno = {nome: "Daniel", idade: 19};
+
+aluno.matricula = 12345;
+aluno.desconto = "Yes";
+aluno.situacao = "Ativa";
+
+console.log(aluno)
+
+//--------------------------------------------------------
+
+var alunoA = {nomeA: "Felipe", 
+                idadeA: 15};
+
+aluno.matricula = 123434;
+
+console.log(alunoA);
+
+//--------------------------------------------------------
+var alunoB = {
+    nomeB: "Renazin",
+    notasB: [8,9]
+};
+
+console.log(alunoB);
