704e7b3633e051bf54f17bdbf012845abb420994
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-08-14 16:03:18 -0300

    Document, alert e demais funções na tela
    Na aula de hoje foram abordados os itens com relação a interação na pagina, como por exemplo alert, windows.alert e demais.

---
+++ Aulas/CONSTLETVAR/script.js
@@ -0,0 +1,25 @@
+let n1 = 150;
+var n2 = 12;
+const n3 = [5,6,2,"Dafoto", "Kalo", "Yaman"];
+
+//Com essa função podemos atribuir um valor, mas sem sobrescrever o que já estava lá
+n3.push(9);
+
+/*
+
+var nome = [
+    nume = "daniel",
+    sobrenome ="Rodrigues",
+    idade = 19
+]
+
+*/
+
+ let soma = n1 * n2 
+
+console.log(n1);
+console.log(n2);
+console.log(n3);
+
+console.log(nome);
+
---
+++ Aulas/DOCUMEENT/script.js
@@ -0,0 +1,1 @@
+document.body.innerHTML = "<h1>Tá rodando no JS</h1>"
