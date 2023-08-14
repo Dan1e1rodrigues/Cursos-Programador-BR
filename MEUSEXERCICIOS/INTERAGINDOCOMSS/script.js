42ba94f025569cf248b91f27b373d19428b3be32
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-08-14 16:36:23 -0300

    Atividade Hora e data
    Nessa atividade foi praticado o conceito de hora, data, mês que e gerada quando  o usuário clica no botão.

---
+++ MEUSEXERCICIOS/INTERAGINDOCOMSS/script.js
@@ -0,0 +1,8 @@
+function ativa() {
+
+    var d = new Date();
+
+    let result = document.querySelector('section#res');
+
+    result.innerHTML = `O que eu recebi do sistema foi <mark>${d}</mark>`
+}
