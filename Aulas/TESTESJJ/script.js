2dc41594c90ef106e5d2935ce323f5ec686ba738
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-12-05 15:56:30 -0300

    UP

--- Aulas/TESTESJJ/script.js
+++ Aulas/TESTESJJ/script.js
@@ -1,7 +1,33 @@
-const a = 'Hello';
-const b = 'world';
-// write your code below
+// const idade = 18;
+// const temAcesso = idade > 18;
 
-let resultString = a ;
+// console.log(temAcesso);
 
-console.log(resultString, b); 
+// if (temAcesso){
+//     console.log('Acesso garantido !');
+// }else {
+//     console.log('Acesso negado !');
+// }
+
+// Operadores logicos AND E OR
+// const idade = 16;
+
+// let verifica = idade >= 16
+
+// console.log(verifica)
+// const dinheiro = 450;
+
+// const maiorDeIdade = idade >= 18;
+
+// console.log(maiorDeIdade);
+
+// const dinheiroSuficiente = dinheiro >= 500;
+
+// console.log(dinheiroSuficiente);
+
+// const pessoaTirarAcarteira = maiorDeIdade && dinheiroSuficiente;
+
+// console.log(pessoaTirarAcarteira);
+
+let pr = 
+
