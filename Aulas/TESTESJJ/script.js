e841f6a0d216e3744a16ccea8526d043d4c59a9d
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-12-12 19:29:47 -0300

    up

--- Aulas/TESTESJJ/script.js
+++ Aulas/TESTESJJ/script.js
@@ -1,11 +1,11 @@
-const precoDocombustivel = 5.79;
+// const precoDocombustivel = 5.79;
 
-const kmPorLitros = 1;
+// const kmPorLitros = 1;
 
-const distanciaEmKm = 100;
+// const distanciaEmKm = 100;
 
-const litrosConsumidos = distanciaEmKm / kmPorLitros
-const valorGasto = litrosConsumidos * precoDocombustivel;
+// const litrosConsumidos = distanciaEmKm / kmPorLitros
+// const valorGasto = litrosConsumidos * precoDocombustivel;
 
-console.log(valorGasto.toFixed(2));
+// console.log(valorGasto.toFixed(2));
 
