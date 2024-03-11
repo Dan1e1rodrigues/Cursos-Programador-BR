d14bd0ac272cc0e0ac69d492eb599ef67380b3d4
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2024-03-11 16:13:02 -0300

    UP

---
+++ Aulas/IFRAME/script.js
@@ -0,0 +1,19 @@
+let jogador = document.getElementById("jogador");
+
+let xInicial = 0;
+let yInicial = 0;
+
+function moverJogadorPara(x,y){
+
+    let posX = x + "px";
+    let posY = y + "px";
+
+    jogador.style.top = posX;
+    jogador.style.left = posY;
+}
+
+setInterval(function(){
+
+    moverJogadorPara(xInicial++, yInicial++);
+
+}, 20);
