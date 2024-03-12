08597d74064b9b270b40e5a7c31aae343e0a55bd
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2024-03-12 15:40:39 -0300

    UP

--- Aulas/IFRAME/script.js
+++ Aulas/IFRAME/script.js
@@ -1,19 +1,40 @@
-let jogador = document.getElementById("jogador");
+// let jogador = document.getElementById("jogador");
 
-let xInicial = 0;
-let yInicial = 0;
+// let xInicial = 0;
+// let yInicial = 0;
 
-function moverJogadorPara(x,y){
+// function moverJogadorPara(x,y){
 
-    let posX = x + "px";
-    let posY = y + "px";
+//     let posX = x + "px";
+//     let posY = y + "px";
 
-    jogador.style.top = posX;
-    jogador.style.left = posY;
-}
+//     jogador.style.top = posX;
+//     jogador.style.left = posY;
+// }
 
-setInterval(function(){
+// setInterval(function(){
 
-    moverJogadorPara(xInicial++, yInicial++);
+//     moverJogadorPara(xInicial++, yInicial++);
 
-}, 20);
+// }, 20);
+
+let tela = document.getElementById("tela");
+
+let ctx = tela.getContext("2d");
+
+ctx.rect(10, 10, 100, 200);
+
+ctx.fillStyle = "blue";
+
+ctx.lineWidth = 5;
+ctx.strokeStyle = "red";
+// ctx.moveTo(0,0);
+// ctx.lineTo(250,250);
+// ctx.lineWidth = 5; aumentar o tamanho da(s) linha(s)
+// ctx.lineTo(500,0);
+// ctx.stroke();
+
+ctx.fill();
+ctx.stroke();
+
+ctx.clearRect(20, 20, 30, 30);
