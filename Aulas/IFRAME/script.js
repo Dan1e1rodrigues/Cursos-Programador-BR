5c02552763beab927464a5ce664e91495e75960e
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2024-03-13 17:27:20 -0300

    up

--- Aulas/IFRAME/script.js
+++ Aulas/IFRAME/script.js
@@ -18,23 +18,41 @@
 
 // }, 20);
 
+//Fazendo o circulo inicio
 let tela = document.getElementById("tela");
 
 let ctx = tela.getContext("2d");
 
-ctx.rect(10, 10, 100, 200);
+let x = 250;
+let y = 250;
+let raio = 100;
 
-ctx.fillStyle = "blue";
+let inicio = 0;
+let fim = 2 * Math.PI;
 
-ctx.lineWidth = 5;
+// Fazendo um circulo fim
+ctx.beginPath();
 ctx.strokeStyle = "red";
-// ctx.moveTo(0,0);
-// ctx.lineTo(250,250);
-// ctx.lineWidth = 5; aumentar o tamanho da(s) linha(s)
-// ctx.lineTo(500,0);
+ctx.fillStyle = "green";
+ctx.arc(x,y,raio,inicio,fim);
+
+ctx.stroke();
+ctx.fill()
+
+
+// ctx.rect(10, 10, 100, 200);
+
+// ctx.fillStyle = "blue";
+
+// ctx.lineWidth = 5;
+// ctx.strokeStyle = "red";
+// // ctx.moveTo(0,0);
+// // ctx.lineTo(250,250);
+// // ctx.lineWidth = 5; aumentar o tamanho da(s) linha(s)
+// // ctx.lineTo(500,0);
+// // ctx.stroke();
+
+// ctx.fill();
 // ctx.stroke();
 
-ctx.fill();
-ctx.stroke();
-
-ctx.clearRect(20, 20, 30, 30);
+// ctx.clearRect(20, 20, 30, 30);
